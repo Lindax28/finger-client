94bb8548ce8b29bb951d673fd208c1bb96500777
Linda Xiao <Lindax28@hotmail.com>
2021-10-07 15:56:08 -0700

    Check for ASCII format and read data into a variable-sized slice

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	"net"
 	"os"
 	"strings"
+	"unicode"
 )
 
 func main() {
+	// Verify number of arguments is correct
 	if len(os.Args) != 2 {
 		fmt.Println("Proper Usage is: ./finger user@hostname")
 		os.Exit(1)
 	}
-
+	// Verify format is username + '@' + hostname
 	args := strings.Split(os.Args[1], "@")
 	if len(args) != 2 {
 		fmt.Println("Proper Usage is: ./finger user@hostname")
@@ -20,15 +22,31 @@ func main() {
 	}
 	user := args[0]
 	hostname := args[1]
-
+	// Verify username is in ASCII format
+	for _, char := range user {
+		if char > unicode.MaxASCII {
+			fmt.Println("Username must be in ASCII format")
+			os.Exit(1)
+		}
+	}
+	// Connect to host on port 79
 	conn, err := net.Dial("tcp", hostname+":79")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer conn.Close()
+	// Write user data to the connection, ended with CRLF
 	conn.Write([]byte(user + "\r\n"))
-	response := make([]byte, 4096)
-	conn.Read(response)
+	// Read data from the connection
+	response := make([]byte, 0)
+	tmp := make([]byte, 256)
+	for {
+		n, err := conn.Read(tmp)
+		if err != nil {
+			break
+		}
+		response = append(response, tmp[:n]...)
+	}
 	fmt.Println(string(response))
 }
